3ba18d2a82b9f16aece90ffc3b33b7f97c6bcf69
kharissulistiyo <kharisblank@gmail.com>
2013-11-06 09:19:03 +0700

    Delete unnecessary files for gh-page

--- sidebar.php
+++
@@ -1,9 +0,0 @@
-			<div id="sidebar">
-			
-			<?php
-				if(function_exists('dynamic_sidebar') && dynamic_sidebar('Widget')) : ?>				
-			<?php	endif; ?>	
-				
-			</div><!--/#sidebar-->	
-			
-			<div class="clear"></div>
